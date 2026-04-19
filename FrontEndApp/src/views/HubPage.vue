483c823a3f654627b997ae50fff8c733c29be4d1
LeoSolo <leo.solo@mail.ru>
2026-04-19 22:19:41 +0700

    pinia + scss

--- FrontEndApp/src/views/HubPage.vue
+++ FrontEndApp/src/views/HubPage.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>HUB-PAGE</h1>
+  <div class='hub-container'>
+    <div class='action-name'>Вылазка</div>
+    <div class='action-name'>Торговец оружием</div>
+    <div class='action-name'>Торговец бижутерией</div>
+    <div class='action-name'>Торговец броней</div>
+    <div class='action-name'>Разведчик</div>
+    <div class='action-name'>Настройки</div>
+    <div class='action-name'>Выход</div>
+  </div>
 </template>
 
-<script setup lang="ts">
-
+<script setup lang='ts'>
 </script>
 
-<style scoped>
-h1 {
-    color: white;
+<style scoped lang='scss'>
+.action-name {
+  color: white;
 }
 </style>
--- FrontEndApp/vite.config.ts
+++ FrontEndApp/vite.config.ts
@@ -1,22 +1,29 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from "path";
+import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => ({
-    build: {
-        outDir: './../dist',
-        chunkSizeWarningLimit: 1000,
+  build: {
+    outDir: './../dist',
+    chunkSizeWarningLimit: 1000,
+  },
+  base: mode == 'development' ? '' : './',
+  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@views': resolve(__dirname, 'src/views'),
     },
-    base: mode == 'development' ? '' : './',
-    plugins: [vue()],
-    server: {
-        port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables.scss" as *;`,
+      },
     },
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-            "@components": resolve(__dirname, "src/components"),
-            "@views": resolve(__dirname, "src/views"),
-        },
-    },
+  },
 }));
